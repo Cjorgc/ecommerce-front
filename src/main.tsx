d2f8c4aa2db9936e7f7656f7c720915ada1f5626
Miguel <njobby_987@hotmail.com>
2023-03-22 17:25:26 -0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, Route, Routes } from "react-router-dom";
-import SignUp from "./pages/Signup";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Login from "./pages/Login";
 import CatalogPage from "./pages/Catalog/CatalogPage";
 import "./styles.css";
-import Header from "./components/Header";
-import CartPage from "./pages/cart/CartPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "catalog",
+        element: <CatalogPage />
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-            <HashRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={ <Home/> } />
-                    <Route path="/about" element={ <About/> }/>
-                    <Route path="/signup" element={ <SignUp/> }/>
-                    <Route path="/login" element={ <Login/> }/>
-                    <Route path="/catalog" element={ <CatalogPage/> }/>
-                    <Route path="/cart" element={ <CartPage/> }/>
-                </Routes>
-            </HashRouter>
+        <RouterProvider router={router}/>
+            {/*<HashRouter>*/}
+            {/*    <Header />*/}
+            {/*    <Routes>*/}
+            {/*        <Route path="/" element={ <Home/> } />*/}
+            {/*        <Route path="/about" element={ <About/> }/>*/}
+            {/*        <Route path="/signup" element={ <SignUp/> }/>*/}
+            {/*        <Route path="/login" element={ <Login/> }/>*/}
+            {/*        <Route path="/catalog" element={ <CatalogPage/> }/>*/}
+            {/*        <Route path="/cart" element={ <CartPage/> }/>*/}
+            {/*    </Routes>*/}
+            {/*</HashRouter>*/}
     </React.StrictMode>,
 )
