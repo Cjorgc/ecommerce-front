fdc6d85998718782300e5bd2917ba78b2aecaa15
Miguel <njobby_987@hotmail.com>
2023-03-22 17:29:31 -0300

    fix test

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CatalogPage from "./pages/Catalog/CatalogPage";
 import "./styles.css";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "catalog",
-        element: <CatalogPage />
-    }
-]);
+import Header from "./components/Header";
+import About from "./pages/About";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
+import CartPage from "./pages/cart/CartPage";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
-            {/*<HashRouter>*/}
-            {/*    <Header />*/}
-            {/*    <Routes>*/}
-            {/*        <Route path="/" element={ <Home/> } />*/}
-            {/*        <Route path="/about" element={ <About/> }/>*/}
-            {/*        <Route path="/signup" element={ <SignUp/> }/>*/}
-            {/*        <Route path="/login" element={ <Login/> }/>*/}
-            {/*        <Route path="/catalog" element={ <CatalogPage/> }/>*/}
-            {/*        <Route path="/cart" element={ <CartPage/> }/>*/}
-            {/*    </Routes>*/}
-            {/*</HashRouter>*/}
+        <HashRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={ <Home/> } />
+                <Route path="/about" element={ <About/> }/>
+                <Route path="/signup" element={ <SignUp/> }/>
+                <Route path="/login" element={ <Login/> }/>
+                <Route path="/catalog" element={ <CatalogPage/> }/>
+                <Route path="/cart" element={ <CartPage/> }/>
+            </Routes>
+        </HashRouter>
     </React.StrictMode>,
 )
